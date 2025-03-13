4c24f27c12fb8f5d008fdf01836f24ec49148b51
eunbb <fakesilverpark@gmail.com>
2025-03-13 23:55:01 +0900

    250313 :

--- PracticeReact/practice1/src/App.jsx
+++ PracticeReact/practice1/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <div className = {darkmode ? "light container" : "dark container"}>
-        <h1>{darkmode ? "Light💡" : "DARK🌙"}Mode</h1>
+        <h1>{darkmode ? "Light💡" : "Dark🌙"}Mode</h1>
         <p>현재 모드는 {darkmode ? "라이트모드" : "다크모드🌙"} 입니다.</p>
         <button className='toggle-btn' onClick={()=>setDarkMode(!darkmode)}>Light Change</button>
       </div>
